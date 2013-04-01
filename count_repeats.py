0a270d922e2ded80e55f5ed1ed147ef2273c5397
ejm2095 <ejm2095@rit.edu>
2013-04-01 10:46:37 -0400

    init

--- count_repeats.py
+++ count_repeats.py
@@ -1,5 +1,5 @@
 
-def repeats(lst):
+def repeatsNN(lst):
     awn = 0
     i =0
     while i < len(lst):
@@ -9,10 +9,26 @@ def repeats(lst):
         i += p
     return awn
 
+def repeatsN(lst):
+    awn = 0
+    i =0
+    d = {}
+    while i < len(lst):
+        x = lst[i]
+        p = d.get(x)
+        if not p:
+            d[x] = 1
+        if p == 1:
+            awn += 1
+            d[x] +=1
+        i += 1
+    return awn
+
 
 l = [1,2,2,3,4,4,4,5,6]
 
-print repeats(l)
+print repeatsNN(l)
+print repeatsN(l)
 
 
 
